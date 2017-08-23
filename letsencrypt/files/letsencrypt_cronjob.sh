36faf653ddaddfa419dd72c563b2b9363abe8c5a
Heinz Wiesinger <heinz@m2mobi.com>
2017-08-23 15:00:44 +0200

    Inly sync 'archive' and 'live' to other hosts.

--- letsencrypt/files/letsencrypt_cronjob.sh
+++ letsencrypt/files/letsencrypt_cronjob.sh
@@ -10,7 +10,8 @@
 
 {%- set mine_query = "G@ec2_tags:account:" + account + " and G@ec2_tags:environment:" + environment + " and G@ec2_tags:role:webserver and G@ec2_tags:purpose:frontend and G@ec2_tags:hierarchy:secondary" %}
 {%- for minion, hostname in salt['mine.get'](mine_query, 'name', 'compound').items() %}
-rsync -lr -e 'ssh -i /home/letsencrypt/.ssh/id_rsa' /etc/letsencrypt/ letsencrypt@{{ hostname }}:/etc/letsencrypt/
+rsync -lr -e 'ssh -i /home/letsencrypt/.ssh/id_rsa' /etc/letsencrypt/archive letsencrypt@{{ hostname }}:/etc/letsencrypt/
+rsync -lr -e 'ssh -i /home/letsencrypt/.ssh/id_rsa' /etc/letsencrypt/live letsencrypt@{{ hostname }}:/etc/letsencrypt/
 {%- endfor %}
 
 {{ webserver_start if webserver_start is defined else '' -}}
